3d7165549e85e00631bf20f9c86c16c711f85026
ChriswFoster <chriswfoster@gmail.com>
2018-03-06 16:39:17 -0600

    Imported onoff into inex

--- server/iotIndex.js
+++ server/iotIndex.js
@@ -1,2 +1,3 @@
 var Gpio = require('onoff').Gpio
 var LED = new Gpio(4, 'out') //use GPIO pin 4, change the 4 to respecify the pin number
+var blinkInterval = setInterval(blinkLED, 250); // this sets the blink interval to 250ms
