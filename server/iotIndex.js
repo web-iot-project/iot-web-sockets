7ef72faf98eed1b992609a4a7ac3bfbfee1304d3
Chris <chriswfoster@gmail.com>
2018-03-17 23:43:47 -0500

    Pure function, but on 2nd socket write it crashes on pi side

--- server/iotIndex.js
+++ server/iotIndex.js
@@ -11,15 +11,62 @@ const io = socketIo(server)
 const port = 3456
 ////////////////////// HI MATT!!! I moved the raspberry pi's IOT info to it's own repository, hit me up if you have any questions. 
 // I'm still moving forward with this project though. Just a dfferent way of organizing the project.
-
-
-
 app.use(cors())
 app.use(bodyParser.json())
 
+let interval;
+io.on("connection", socket => {
+    console.log("hi")
+    if (interval) {
+        clearInterval(interval);
+      }
+  intervalId = setInterval(() => getApiAndEmit(socket), 5000)
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected")
+    clearInterval(intervalId)
+  })
+})
+
+const lightOn = true;
+
+const getApiAndEmit = async socket => {
+  try {
+    const lightSend = lightOn
+    socket.emit("FromServer", lightSend)
+    // console.log(sd1obj)
+  } catch (error) {
+    console.error(`Error: ${error.code}`)
+  }
+}
 
 
 
-app.listen(port, ()=> console.log(`Listening on port: ${port}`))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+server.listen(port, () => console.log(`Listening on port ${port}`))
+
+// app.listen(port, ()=> console.log(`Listening on port: ${port}`))
 
 
