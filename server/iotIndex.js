e5ac8c8c15e4eeffd77da8ac819c6039b5415e11
Chris <chriswfoster@gmail.com>
2018-03-17 21:35:18 -0500

    Put in blinkLED functions

--- server/iotIndex.js
+++ server/iotIndex.js
@@ -4,3 +4,20 @@ var blinkInterval = setInterval(blinkLED, 250); // this sets the blink interval 
 const express = require ('express');
 const cors = require('cors');
 const bodyParser = require('body-parser')
+
+
+function blinkLED() { //function to start blinking
+    if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+      LED.writeSync(1); //set pin state to 1 (turn LED on)
+    } else {
+      LED.writeSync(0); //set pin state to 0 (turn LED off)
+    }
+  }
+  
+  function endBlink() { //function to stop blinking
+    clearInterval(blinkInterval); // Stop blink intervals
+    LED.writeSync(0); // Turn LED off
+    LED.unexport(); // Unexport GPIO to free resources
+  }
+  
+  setTimeout(endBlink, 5000); //stop blinking after 5 seconds
